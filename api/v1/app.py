a0c0f583699ddabb0454587e52aeaeefb19c0acd
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-29 00:47:45 +0300

    api error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,24 @@
 """Status of your API"""
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """storage handling"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Jsonified error handler"""
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 
 
 if __name__ == '__main__':
