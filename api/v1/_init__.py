37effc5275703c7bf0cf574641c641f7fe724722
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-31 16:39:24 +0300

    e

--- api/v1/_init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-Initialization file for v1"""
-
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='api/v1')
-
-from ap1.v1.views.index import *
